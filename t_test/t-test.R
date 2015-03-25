3b13e07ef1dc39ab2d3abebcc22b21c197bd0884
Daniel G Travieso <danielgtravieso@gmail.com>
2015-03-25 16:25:33 -0300

    made the error handling for anova

--- t_test/t-test.R
+++ t_test/t-test.R
@@ -38,6 +38,9 @@ if (options$type == "lfqlog2") {
   code <- "MS";
 }
 
+if (!(TRUE %in% grepl(regexpr, colnames(table)))
+  sprintf("Error: No columns of type %s in input table", code);
+  q(1,s="no");
 # define the columns that will be taken in account for the t-test
 columns_names <- grep(regexpr, colnames(table), value=TRUE);
 
