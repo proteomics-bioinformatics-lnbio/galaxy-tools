a887f80a7659983eb065d3adb3053df9015dcff0
Daniel Travieso <danielgtravieso@gmail.com>
2015-10-15 11:18:30 -0300

    minor changes

--- marshal/marshal.R
+++ marshal/marshal.R
@@ -26,6 +26,7 @@ opt = matrix(c(
 options = getopt(opt);
 
 table <- read.delim(options$inputfile_name, header=TRUE, fill=TRUE);
+
 db.socket <- '/var/run/mysqld/mysqld.sock';
 #Database connections and queries
 db.connection <- dbConnect(RMySQL::MySQL(), user='galaxy', host='localhost', dbname='conversionMarcelo', password='123456', unix.sock=db.socket);
@@ -79,124 +80,124 @@ cell.tax <- options$tax_id;
 #finding the id type
 rows.excluded <- c();
 for (row in seq(2, nrow(table))) {
-    row.deleted <- FALSE;
-    cell.id.notfound <- FALSE;
-    cell.row <- row;
-    cell.value <- strsplit(table[cell.row, column_names.proteinIDs], ';')[[1]];
-                                        #print("Current table row:");
-                                        #print(cell.value);
-    cell.id <- "";
-    for (id_code in cell.value) {
-                                        # remove contaminant or reversed protein ids from search. get the first valid id
-        if(!is.na(id_code) && !grepl(regex.id.contaminant_reversed, id_code)) {
-            cell.id <- id_code;
+  row.deleted <- FALSE;
+  cell.id.notfound <- FALSE;
+  cell.row <- row;
+  cell.value <- strsplit(table[cell.row, column_names.proteinIDs], ';')[[1]];
+  #print("Current table row:");
+  #print(cell.value);
+  cell.id <- "";
+  for (id_code in cell.value) {
+    # remove contaminant or reversed protein ids from search. get the first valid id
+    if(!is.na(id_code) && !grepl(regex.id.contaminant_reversed, id_code)) {
+      cell.id <- id_code;
+      break;
+    }
+  }
+  if (cell.id == "") {
+
+    cell.id.notfound <- TRUE;
+    # NOT FOUND ANY RELEVANT IDs (All contaminat or reversed)
+    if (options$keepcon == "Yes") {
+      cell.id <- grep(regex.id.contaminant, cell.value, value=TRUE)[1];
+      if (is.na(cell.id)) {
+        rows.excluded <- c(rows.excluded, row);
+        row.deleted <- TRUE;
+      } else {
+        cell.id <- gsub(regex.id.contaminant, '', cell.id);
+      }
+    } else {
+      rows.excluded <- c(rows.excluded, row);
+      row.deleted <- TRUE;
+    }
+  }
+  if (!row.deleted) {
+    # discover the type of id
+    if (grepl(regex.id.uniprot.1, cell.id) || grepl(regex.id.uniprot.2, cell.id)) {
+      cell.hash <- 'uniprot';
+    } else if(grepl(regex.id.ipi, cell.id)) {
+      cell.hash <- 'ipi';
+    } else if(grepl(regex.id.tair, cell.id)) {
+      cell.hash <- 'tair';
+    } else if(grepl(regex.id.ensembl, cell.id)) {
+      cell.hash <- 'ensembl';
+    } else if(grepl(regex.id.refseq, cell.id)) {
+      cell.hash <- 'refseq'
+    } else {
+      cell.hash <- 'genesymbol'
+    }
+    # if the id is not uniprot type, get the correlant uniprot for that id
+    if (cell.hash != 'uniprot') {
+      # if is genesymbol use the database to search for a uniprot with same tax number
+      if (cell.hash == 'genesymbol') {
+        cell.id.escapeChars <- dbEscapeStrings(db.connection, cell.id);
+        db.select.all <- paste(db.sql.all, "'", cell.id.escapeChars, "';", sep='');
+        db.select.results <- dbGetQuery(db.connection, db.select.all);
+
+        for (rrow in seq(1, nrow(db.select.results))) {
+          db.select.results.row <- db.select.results[row,];
+
+          if (db.select.results.row[[4]] == cell.tax) {
+            cell.id.uniprot <- db.select.results.row[[2]];
+            if (db.select.results.row[[5]] == "YES") {
+              break;
+            }
+          }
+        }
+        # if is not genesymbol, query for all ids in the table, and get the result uniprot
+      } else {
+        cell.id.escapeChars <- dbEscapeStrings(db.connection, cell.id);
+        db.select.uniprot <- paste(db.sql.uniprot, "'", cell.id.escapeChars, "';", sep='');
+        db.select.results <- dbGetQuery(db.connection, db.select.uniprot);
+        db.select.results.ids <- as.vector(db.select.results[[1]]);
+        for (res in db.select.results.ids) {
+          if (grepl(res, regex.id.uniprot.1) == TRUE ||
+          grepl(res, regex.id.uniprot.2) == TRUE) {
+            cell.id.uniprot <- res;
             break;
+          }
         }
-    }
-    if (cell.id == "") {
-
-        cell.id.notfound <- TRUE;
-        # NOT FOUND ANY RELEVANT IDs (All contaminat or reversed)
-        if (options$keepcon == "Yes") {
-            cell.id <- grep(regex.id.contaminant, cell.value, value=TRUE)[1];
-            if (is.na(cell.id)) {
-                rows.excluded <- c(rows.excluded, row);
-                row.deleted <- TRUE;
-            } else {
-                cell.id <- gsub(regex.id.contaminant, '', cell.id);
-            }
+      }
+      # write the uniprot conversion cell
+      if (cell.id.notfound) {
+        cell.id <- paste0("CON__", cell.id);
+      }
+      table[cell.row, column_names.proteinIDs] <- cell.id;
+      table[cell.row, column_names.uniprot_conversion] <- paste0(cell.id, "_", cell.id.uniprot);
+    } else {
+      # if the id is already uniprot, do nothing and store the uniprot id value
+      cell.id.uniprot <- cell.id;
+      cell.id <- "";
+      cell.id.possibleid <- c();
+      cell.id.uniprot <- sub('-[[:digit:]]', '', cell.id.uniprot);
+      cell.id.escapeChars <- dbEscapeStrings(db.connection, cell.id.uniprot);
+      db.select.synonym <- paste(db.sql.synonym, "'", cell.id.escapeChars, "';", sep='');
+      db.select.results <- dbGetQuery(db.connection, db.select.synonym);
+      db.select.results.ids <- as.vector(db.select.results[[1]]);
+      for (res in db.select.results.ids) {
+        cell.id.possibleid <- c(cell.id.possibleid, res);
+        if (!grepl('^IPI|^ENS|^A[Tt]',res)) {
+          cell.id <- res;
+          break;
+        }
+      }
+      if (cell.id == "") {
+        if (!is.null(cell.id.possibleid)) {
+          cell.id <- sort(cell.id.possibleid)[1]
         } else {
-            rows.excluded <- c(rows.excluded, row);
-            row.deleted <- TRUE;
+          cell.id <- cell.id.uniprot;
         }
+      }
+      # write the uniprot conversion cell
+      if (cell.id.notfound) {
+        table[cell.row, column_names.proteinIDs] <- paste0("CON__", cell.id.uniprot);
+        table[cell.row, column_names.uniprot_conversion] <- paste0("CON__", cell.id, "_", cell.id.uniprot);
+      } else {
+        table[cell.row, column_names.proteinIDs] <- cell.id.uniprot;
+        table[cell.row, column_names.uniprot_conversion] <- paste0(cell.id, "_", cell.id.uniprot);
+      }
     }
-    if (!row.deleted) {
-        # discover the type of id
-        if (grepl(regex.id.uniprot.1, cell.id) || grepl(regex.id.uniprot.2, cell.id)) {
-            cell.hash <- 'uniprot';
-        } else if(grepl(regex.id.ipi, cell.id)) {
-            cell.hash <- 'ipi';
-        } else if(grepl(regex.id.tair, cell.id)) {
-            cell.hash <- 'tair';
-        } else if(grepl(regex.id.ensembl, cell.id)) {
-            cell.hash <- 'ensembl';
-        } else if(grepl(regex.id.refseq, cell.id)) {
-            cell.hash <- 'refseq'
-        } else {
-            cell.hash <- 'genesymbol'
-        }
-        # if the id is not uniprot type, get the correlant uniprot for that id
-        if (cell.hash != 'uniprot') {
-            # if is genesymbol use the database to search for a uniprot with same tax number
-            if (cell.hash == 'genesymbol') {
-                cell.id.escapeChars <- dbEscapeStrings(db.connection, cell.id);
-                db.select.all <- paste(db.sql.all, "'", cell.id.escapeChars, "';", sep='');
-                db.select.results <- dbGetQuery(db.connection, db.select.all);
-
-                for (rrow in seq(1, nrow(db.select.results))) {
-                    db.select.results.row <- db.select.results[row,];
-
-                    if (db.select.results.row[[4]] == cell.tax) {
-                        cell.id.uniprot <- db.select.results.row[[2]];
-                        if (db.select.results.row[[5]] == "YES") {
-                            break;
-                        }
-                    }
-                }
-                # if is not genesymbol, query for all ids in the table, and get the result uniprot
-            } else {
-                cell.id.escapeChars <- dbEscapeStrings(db.connection, cell.id);
-                db.select.uniprot <- paste(db.sql.uniprot, "'", cell.id.escapeChars, "';", sep='');
-                db.select.results <- dbGetQuery(db.connection, db.select.uniprot);
-                db.select.results.ids <- as.vector(db.select.results[[1]]);
-                for (res in db.select.results.ids) {
-                    if (grepl(res, regex.id.uniprot.1) == TRUE ||
-                        grepl(res, regex.id.uniprot.2) == TRUE) {
-                        cell.id.uniprot <- res;
-                        break;
-                    }
-                }
-            }
-            # write the uniprot conversion cell
-            if (cell.id.notfound) {
-                cell.id <- paste0("CON__", cell.id);
-            }
-            table[cell.row, column_names.proteinIDs] <- cell.id;
-            table[cell.row, column_names.uniprot_conversion] <- paste0(cell.id, "_", cell.id.uniprot);
-        } else {
-            # if the id is already uniprot, do nothing and store the uniprot id value
-            cell.id.uniprot <- cell.id;
-            cell.id <- "";
-            cell.id.possibleid <- c();
-            cell.id.uniprot <- sub('-[[:digit:]]', '', cell.id.uniprot);
-            cell.id.escapeChars <- dbEscapeStrings(db.connection, cell.id.uniprot);
-            db.select.synonym <- paste(db.sql.synonym, "'", cell.id.escapeChars, "';", sep='');
-            db.select.results <- dbGetQuery(db.connection, db.select.synonym);
-            db.select.results.ids <- as.vector(db.select.results[[1]]);
-            for (res in db.select.results.ids) {
-                cell.id.possibleid <- c(cell.id.possibleid, res);
-                if (!grepl('^IPI|^ENS|^A[Tt]',res)) {
-                    cell.id <- res;
-                    break;
-                }
-            }
-            if (cell.id == "") {
-                if (!is.null(cell.id.possibleid)) {
-                    cell.id <- sort(cell.id.possibleid)[1]
-                } else {
-                    cell.id <- cell.id.uniprot;
-                }
-            }
-            # write the uniprot conversion cell
-            if (cell.id.notfound) {
-                table[cell.row, column_names.proteinIDs] <- paste0("CON__", cell.id.uniprot);
-                table[cell.row, column_names.uniprot_conversion] <- paste0("CON__", cell.id, "_", cell.id.uniprot);
-            } else {
-                table[cell.row, column_names.proteinIDs] <- cell.id.uniprot;
-                table[cell.row, column_names.uniprot_conversion] <- paste0(cell.id, "_", cell.id.uniprot);
-            }
-        }
-    }
+  }
 }
 table[-1, -c(1, 2)] <- apply(table[-c(1),-c(1, 2)], 2, as.numeric);
 table <- table[-rows.excluded,];
